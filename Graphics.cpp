d011c6442408df299bf0a5cc770612e5a7b5256e
bpucilowski <pucilowskibartlomiej@gmail.com>
2024-08-27 12:54:13 +0200

    Change to newer swap effect, unbind shadow map texture after render

--- Graphics.cpp
+++ Graphics.cpp
@@ -6,20 +6,20 @@
 Graphics::Graphics(const HWND& hWnd, const unsigned int windowWidth, const unsigned int windowHeight)
 {
 	DXGI_SWAP_CHAIN_DESC scd = {};
-	scd.BufferDesc.Width = 0;
-	scd.BufferDesc.Height = 0;
+	scd.BufferDesc.Width = 0u;
+	scd.BufferDesc.Height = 0u;
 	scd.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-	scd.BufferDesc.RefreshRate.Numerator = 0;
-	scd.BufferDesc.RefreshRate.Denominator = 0;
+	scd.BufferDesc.RefreshRate.Numerator = 0u;
+	scd.BufferDesc.RefreshRate.Denominator = 0u;
 	scd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
 	scd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
-	scd.SampleDesc.Count = 1;
+	scd.SampleDesc.Count = 1u;
 	scd.SampleDesc.Quality = 0;
 	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	scd.BufferCount = 1;
+	scd.BufferCount = 2u;
 	scd.OutputWindow = hWnd;
 	scd.Windowed = TRUE;
-	scd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	scd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	scd.Flags = 0;
 
 	UINT swapCreateFlags = 0u;
@@ -169,6 +169,8 @@ void Graphics::SetNormalRenderTarget()
 void Graphics::EndFrame()
 {
 	CHECK_HR(swapChain->Present(1u, 0u));
+	ID3D11ShaderResourceView* nullSRV[1] = { nullptr };
+	context->PSSetShaderResources(0, 1, nullSRV);
 }
 
 void Graphics::SetProjection(const DirectX::XMMATRIX proj) noexcept
